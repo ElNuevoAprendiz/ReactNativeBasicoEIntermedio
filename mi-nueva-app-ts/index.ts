e6bce488e0573ad9e7e65449832344cf856b2275
ElNuevoAprendiz! <profedanieljuan@gmail.com>
2025-11-18 09:32:49 -0300

    Se traduce una explicación en el archivo index acerca del uso de registerRootComponent

--- mi-nueva-app-ts/index.ts
+++ mi-nueva-app-ts/index.ts
@@ -5,4 +5,9 @@ import App from './App';
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+
+// registerRootComponent llama a AppRegistry.registerComponent('main', () => App);
+// También garantiza que, independientemente de si se carga la aplicación en Expo Go o en una compilación nativa,
+// el entorno esté configurado correctamente
+
 registerRootComponent(App);
